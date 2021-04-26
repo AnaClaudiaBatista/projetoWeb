26420bcf0d793812f339e307c3f8c0277d81221e
Ana Claudia <48568934+AnaClaudiaBatista@users.noreply.github.com>
2021-04-26 14:44:31 -0300

    .

--- projetoWebll/remove_cliente.php
+++ projetoWebll/remove_cliente.php
@@ -1,14 +1,14 @@
 <?php
 include_once "fachada.php";
 
-$clienteid = @$_GET["clienteid"];
+$clienteid      = @$_GET["clienteid"];
 
 $cliente = new Cliente($clienteid, null, null, null,null, null);
 $dao = $factory->getClienteDao();
 $dao->removePorId($clienteid);
 
 
-//header("Location: consultaClientes.php");
+header("Location: consultaClientes.php");
 
 
 exit;
